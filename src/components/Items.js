a8a2d5987e327f4c5fc4fbb419b431da95929132
kirti18patel <kirti18patel@yahoo.com>
2021-10-15 12:16:02 -0400

    data taken from third party api

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,82 +2,52 @@ import React, {useState, useEffect} from 'react'
 import {useDispatch} from "react-redux"
 import { addItem } from '../actions'
 import "../assets/stylesheet/items.css"
+import API from '../api';
 
 function Items() {
     
-  const [items] = useState([
-    {
-      product_name:"Product Name 1",
-      brand_name: "BRAND NAME",
-      price: "900",
-      description: "table"
-    },
-    {
-      product_name:"Product Name 2",
-      brand_name: "BRAND NAME",
-      price: "900",
-      description: "table"
-    },
-    {
-      product_name:"Product Name 3",
-      brand_name: "BRAND NAME",
-      price: "900",
-      description: "table"
-    },
-    {
-      product_name:"Product Name 4",
-      brand_name: "BRAND NAME",
-      price: "900",
-      description: "table"
-    },
-    {
-      product_name:"Product Name 5",
-      brand_name: "BRAND NAME",
-      price: "900",
-      description: "table"
-    },
-    {
-      product_name:"Product Name 6",
-      brand_name: "BRAND NAME",
-      price: "900",
-      description: "table"
-    }
-  ]);  
+  const [items, setItems] = useState(null);  
   const [cartItems, setCartItems] = useState();
   const dispatch = useDispatch();
+
+  useEffect( async () => {
+    const response = await API.get();
+    setItems(response.data);
+  }, []);
 
   useEffect(() => {
     if(cartItems){
     dispatch(addItem(cartItems))}
-}, [cartItems, dispatch]);
+  }, [cartItems, dispatch]);
 
     return (
         <section className="items flex_center_spaceArround">
-            {items.map((item, i) => (
+            {!items?<h1>loading...</h1> : items.map((item, i) => (
               <div className="item" key={i}>
                 <img
-                  src={require(`../assets/images/${i+1}.jpg`).default}
-                  alt={item.description}
+                  src={item.itemLink}
+                  alt={item.vendorItemDescription}
                   className="item_img"
                   key={`img${i}`}
                 />
                 <div className="overlay flex_spaceBetween flex_column" key={`overlay${i}`}>
                   <div className="item_title flex_spaceBetween flex_column">
-                    <h2 className="item_name">{item.product_name}</h2>
-                    <h4 className="item_brand">{item.brand_name}</h4>
+                    <h2 className="item_name">{item.itemName}</h2>
+                    {/* not getting brand value is api so adding color for now */}
+                    <h4 className="item_brand">{item.color}</h4>
                   </div>
                   <div className="flex_spaceBetween">
                     <button className="add_btn" onClick={()=> {
                       setCartItems({
-                        product_name:item.product_name,
-                        brand_name: item.brand_name,
-                        price: item.price,
-                        description: item.description,
+                        product_name:item.itemName,
+                        brand_name: item.color,
+                        price: item.MSRP,
+                        description: item.vendorItemDescription,
                         id: i
                       })
                     }}>
                         + Add to cart</button>
-                    <h4 className="item_price">${item.price}</h4>
+                    <h4 className="item_price">${item.MSRP}</h4>
                   </div>
                 </div>
               </div>
